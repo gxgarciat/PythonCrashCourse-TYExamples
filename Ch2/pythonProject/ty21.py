352fe1337e801cc0e8d7c09c28770156344cd557
gxgarciat <info@gxgarciat.dev>
2024-01-24 06:14:08 +0000

    Python Practice

---
+++ Ch2/pythonProject/ty21.py
@@ -0,0 +1,4 @@
+# Simple message Assign a message to a variable and then print the message
+
+simMessage = "This is a simple message."
+print(simMessage)
---
+++ Ch2/pythonProject/ty23.py
@@ -0,0 +1,6 @@
+# Personal message: use a variable to represent a person's name and print a message
+# to that person. Your message should be simple
+
+myName = "Eric"
+#perMessage = "Hello Eric, would you like to learn some python today?"
+print(f"Hello {myName}, would you like to learn some Python today?")
