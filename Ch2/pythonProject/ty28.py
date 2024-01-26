71f1270645a27890d370697968e5cd29716a5cb1
gxgarciat <info@gxgarciat.dev>
2024-01-26 07:51:22 +0000

    Python Practice

---
+++ Ch2/pythonProject/ty28.py
@@ -0,0 +1,5 @@
+# Assign the value "python_notes.txt" to a variable called filename. Then use removesuffix() method
+# to display the filename without the file extension.
+
+filename = input("Enter file name: ")
+print(f"The file name without the file extension is: {filename.removesuffix('.txt')}")
