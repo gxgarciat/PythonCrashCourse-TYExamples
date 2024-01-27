e9cc193f6b866c14440f154f54207e375a3d4032
gxgarciat <info@gxgarciat.dev>
2024-01-27 09:25:45 +0000

    Python practice

---
+++ Ch2/pythonProject/ty210.py
@@ -0,0 +1,7 @@
+# Use a variable to represent your favourite number
+# Then, using that variable, create a message that reveals your favourite number
+# Print that message
+
+favouriteNumber = input("What is your favourite number?: ")
+# print(type(favouriteNumber))
+print(f'Amazing! Your favourite number is {favouriteNumber}')
