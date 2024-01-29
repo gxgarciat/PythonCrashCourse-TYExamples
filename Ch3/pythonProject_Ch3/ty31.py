3caaa63cdb6dc1aa3079f9aa4b2c06b10a198fef
gxgarciat <info@gxgarciat.dev>
2024-01-29 03:05:34 +0000

    Python practice

---
+++ Ch3/pythonProject_Ch3/ty31.py
@@ -0,0 +1,2 @@
+# Store the names of a few of your friends in a list called names. Print each person's
+# name by accessing each element in the list, one at a time
---
+++ Ch3/pythonProject_Ch3/ty32.py
@@ -0,0 +1,3 @@
+# Start with the list you used in Exercise 3.1, but instead of just printing each person's name,
+# print a message to them. The text of each message should be the same, but each message shoild be
+# personalized with the person's name.
