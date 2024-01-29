ec888291466dafdf03aa0aa15173be76ea696415
gxgarciat <info@gxgarciat.dev>
2024-01-29 03:09:54 +0000

    Python practice

---
+++ Ch3/pythonProject_Ch3/ty33.py
@@ -0,0 +1,3 @@
+# Your own list: Think of your favourite mode of transportation, such as a motorcycle or a car,
+# and make a list that stores several examples. Use your list to print a series of statements
+# these items, such as "I would like to own a Honda motorcycle.
