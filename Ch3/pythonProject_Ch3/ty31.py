cd5ef2e371188ddd4ae2681fd8aaf977128a676d
gxgarciat <info@gxgarciat.dev>
2024-01-31 21:39:04 +0000

    Python practice

--- Ch3/pythonProject_Ch3/ty31.py
+++ Ch3/pythonProject_Ch3/ty31.py
@@ -1,2 +1,6 @@
 # Store the names of a few of your friends in a list called names. Print each person's
 # name by accessing each element in the list, one at a time
+
+friendNames = ["Andres", "Juan", "Daniel", "Alberto"]
+decisionVar = int(input("Choose one of your friends: 0 - 3: "))
+print(f'Your friend is: {friendNames[decisionVar]}')
