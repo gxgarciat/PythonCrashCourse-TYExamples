272c2973588a234c3e273922d0fd856e19328cb6
gxgarciat <info@gxgarciat.dev>
2024-02-01 21:22:36 +0000

    Python practice

--- Ch3/pythonProject_Ch3/ty32.py
+++ Ch3/pythonProject_Ch3/ty32.py
@@ -1,3 +1,7 @@
 # Start with the list you used in Exercise 3.1, but instead of just printing each person's name,
-# print a message to them. The text of each message should be the same, but each message shoild be
+# print a message to them. The text of each message should be the same, but each message should be
 # personalized with the person's name.
+
+friendNames = ["Andres", "Juan", "Daniel", "Alberto"]
+decisionVar = int(input("Choose one of your friends: 0 - 3: "))
+print(f'Good morning {friendNames[decisionVar]}, I hope you have a lovely evening my friend.')
