c775ce749dbd23ac8653dafe6e372909b8749029
gxgarciat <info@gxgarciat.dev>
2024-02-17 12:41:42 +0000

    Python practice

--- Ch3/pythonProject_Ch3/ty35.py
+++ Ch3/pythonProject_Ch3/ty35.py
@@ -3,4 +3,27 @@
 # 1. Start with your program from exercise 3-4. Add print() call at the end of your program,
 #    stating the name of the guest who can't make it.
 # 2. Modify your list, replacing the name of the guest with the name of the new person you're inviting
-# 3. Print a second
+# 3. Print a second set of invitation messages.
+
+from random import randint
+
+# Part 1
+guessList = ["Gandhi", "Napoleon", "Simon Bolivar", "Messi"]
+
+rd1 = randint(0, len(guessList) - 1)
+print("------Dinner invitation-------")
+print(f'Hi {guessList[0]}, when are you available? I would like to invite you to dinner.')
+print(f'Hi {guessList[1]}, when are you available? I would like to invite you to dinner.')
+print(f'Hi {guessList[2]}, when are you available? I would like to invite you to dinner.')
+print(f'Hi {guessList[3]}, when are you available? I would like to invite you to dinner.')
+print("---------------------------------")
+print(f"We heard that {guessList[rd1]} won't be able to dinner. We will miss you {guessList[rd1]}!")
+print("---------------------------------")
+newPerson = input("Who would you like to invite? ")
+guessList[rd1] = newPerson
+print("")
+print("------Dinner invitation-------")
+print(f'Hi {guessList[0]}, when are you available? I would like to invite you to dinner.')
+print(f'Hi {guessList[1]}, when are you available? I would like to invite you to dinner.')
+print(f'Hi {guessList[2]}, when are you available? I would like to invite you to dinner.')
+print(f'Hi {guessList[3]}, when are you available? I would like to invite you to dinner.')
