0eca15847e3743a7c52556b8c92d0791393e2a99
gxgarciat <info@gxgarciat.dev>
2024-02-17 11:47:14 +0000

    Python practice

--- Ch3/pythonProject_Ch3/ty33.py
+++ Ch3/pythonProject_Ch3/ty33.py
@@ -1,4 +1,12 @@
 # Your own list: Think of your favourite mode of transportation, such as a motorcycle or a car,
 # and make a list that stores several examples. Use your list to print a series of statements
 # these items, such as "I would like to own a Honda motorcycle.
+import random
 
+transportationMode = ["motorcycle", "car", "bike"]
+randomBrands = ["Ford", "Yamaha", "Cervelo", "Harley"]
+
+rd1 = random.randint(0,len(transportationMode)-1)
+rd2 = random.randint(0,len(randomBrands)-1)
+
+print(f'I would like to buy a {randomBrands[rd2]} {transportationMode[rd1]}')
