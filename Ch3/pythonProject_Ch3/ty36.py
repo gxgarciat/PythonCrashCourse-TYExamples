de041b4f83be13c24f25a1953b07ffe022adec39
gxgarciat <info@gxgarciat.dev>
2024-02-18 10:07:29 +0000

    Python practice

--- Ch3/pythonProject_Ch3/ty36.py
+++ Ch3/pythonProject_Ch3/ty36.py
@@ -1,1 +1,7 @@
-#
+# More guests: You just found a bigger dinner table, so more space is available. Think of 3 more guests
+# guests to invite to dinner.
+# 1. Start your program from exercise 3.4 - 3.5. Add a print() call, to inform you found a bigger table
+# 2. Use insert() to add a new guest to the beginning of your list
+# 3. Use insert() to add a new guest to the middle of your list
+# 4. Use append() to add one new guest to the end of your list
+# 5. Print a new set of invitations, one for each person in your list.
