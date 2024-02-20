0e9a55f34d7c5e0c074f1453611622ef327e26ec
gxgarciat <info@gxgarciat.dev>
2024-02-20 04:17:10 +0000

    Python practice

---
+++ Ch3/pythonProject_Ch3/ty37.py
@@ -0,0 +1,6 @@
+# You just found out that your new dinner table wont't arrive in time for the dinner, and
+# now you have space for only two guests
+# 1. Start your program from Exercise 3-6. Add a new line that prints a message saying that you can
+#    invite only two people for dinner
+# 2. Use pop() to remove guests from uyour list one at a time until only two names remain in your list.
+#    Each time
