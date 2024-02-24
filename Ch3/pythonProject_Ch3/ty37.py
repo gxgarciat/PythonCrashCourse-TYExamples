03df996c917782241ba9e9e9810ebeeb0282a68e
gxgarciat <info@gxgarciat.dev>
2024-02-24 09:22:22 +0000

    Python practice

--- Ch3/pythonProject_Ch3/ty37.py
+++ Ch3/pythonProject_Ch3/ty37.py
@@ -7,4 +7,4 @@
 #    are sorry you can't invite them to dinner
 # 3. Print a message to each of the two people still on your list, letting them know they're still invited
 # 4. Use del to remove the last two names from your list, so you have an empty list. Print your list
-#    to make sure you actually have an empty list at the end of your program
+#    to make sure you actually have an empty list at the end of your program.
